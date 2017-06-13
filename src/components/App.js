2ee0633f9aea017302b462be000dbd83be894039
Oleg Gladyshev <molfarr@gmail.com>
2017-06-13 15:03:42 +0300

    entry points

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TriangleIdentifier } from './TriangleIdentifier/TriangleIdentifier';
+
+/**
+ * Root component
+ */
+export class App extends React.Component {
+    render() {
+        return (
+            <TriangleIdentifier/>
+        );
+    }
+}
